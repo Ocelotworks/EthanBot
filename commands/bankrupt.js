7d3e79f803718c5213281d55fcbd0fed317cab1d
Neil Trotter <ntrotter699@gmail.com>
2018-04-24 23:14:33 +0100

    Fixed

--- commands/bankrupt.js
+++ commands/bankrupt.js
@@ -9,16 +9,21 @@ module.exports = {
     run: function run(user, userID, channel, message, args, event, bot) {
 		bot.database.getBalance(userID).then(function(result){
 			if(result[0] && result[0].balance <= 0){
-				bot.database.setBalance(userID, 0);
-				bot.database.addBankruptcy(userID);
-				
-				bot.database.getBankrupt(userID).then(function(result){
-					bot.sendMessage({
-                    to: channel,
-                    message: `:dollar: **${Object.keys(bot.servers[bot.channels[channel].guild_id]).indexOf(userID) > -1 ? "<@"+userID+">" : user}** has been bankrupt ${result[0].bankrupt} times!`
+				bot.database.setBalance(userID, 0).then(function(result){
+					bot.database.addBankruptcy(userID).then(function(result){
+						bot.database.getBankrupt(userID).then(function(result){
+							bot.sendMessage({
+								to: channel,
+								message: `:dollar: **${Object.keys(bot.servers[bot.channels[channel].guild_id]).indexOf(userID) > -1 ? "<@"+userID+">" : user}** has been bankrupt ${result[0].bankrupt} times!`
+							});
+						});
 					});
 				});
-			
+			} else {
+				bot.sendMessage({
+                    to: channel,
+                    message: `:dollar: You have to owe money to declare bankruptcy!`
+				});
 			}
 		})
     }
